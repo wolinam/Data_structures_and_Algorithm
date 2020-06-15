89c045c08e23dca04361f4c8ca3b410bc50dcdf2
Wolinam <zmffhqj35@naver.com>
2020-06-16 03:52:19 +0900

    Update Main.cpp

--- 02__최대_공약수_구하기/Main.cpp
+++ 02__최대_공약수_구하기/Main.cpp
@@ -7,19 +7,20 @@ int find_gcd(int, int);
 int main() {
 	
 	int a,b;
-	cout <<"a=";
-	cin>>a;
+	cout <<"a="; 
+	cin>>a; //1.a입력받기
 	
 	cout <<"b=";
-	cin>>b;
+	cin>>b; //2.b입력받기
 	
-	cout<<"GCD="<<find_gcd(a,b);
+	cout<<"GCD="<<find_gcd(a,b); //3.최대공약수 함수 호출
 	
 	return 0;
 	
 }
+//최대공약수 함수 정의
 int find_gcd(int a, int b){
-	while(a!=b){
+	while(a!=b){ //두 수가 같아질 때까지 계속 빼다가 a 리턴하기
 		if (a>b)
 			a = a-b;
 		else
