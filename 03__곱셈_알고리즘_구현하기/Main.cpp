d16edf412340276f27d498fd4369b42aab49e1be
Wolinam <zmffhqj35@naver.com>
2021-06-30 16:47:59 +0900

    Add files via upload

---
+++ 03__곱셈_알고리즘_구현하기/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    int result=0;
+    cout<<"input x=";
+    cin >> x;
+    cout<<"input y=";
+    cin >> y;
+//필요한 코드를 작성하시오
+		result = x*y;	
+	
+	
+	
+	cout<<"Result:"<<result<<endl;
+
+}
